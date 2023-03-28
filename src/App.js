ef4769a3a4fd229f1caf9bb47656ba8b2cac55fb
arwrdi <arwinrnrdi@gmail.com>
2023-03-28 21:04:52 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+
+import * as React from 'react';
+import './App.css'
+function Board() {
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [isX, setIsX] = React.useState(true);
+  const selectSquare = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    
+    squares[i] = isX ? 'X' : 'O'
+    setSquares(squares)
+    setIsX(!isX)
+  }
+
+  const winner = calculateWinner(squares)
+  let status
+  
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = 'Next player: ' + (isX ? 'X' : 'O');
+  }
+
+  function restart() {
+    setIsX(true)
+    setSquares(Array(9).fill(null))
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      <div className= "board"></div>
+      <div className= "board-row" >
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className= "board-row" >
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className= "board-row" >
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div className="status">{status}</div>
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div >
+      <div >
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+      ? `Scratch: Cat's game`
+      : `Next player: ${nextValue}`;
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function App() {
+  return <Game />;
+}
+
+export default App;
